d84f5c5470e849096d8e2658d24172319fea59fb
ABIN <abinjosepala@gmail.com>
2026-09-22 10:25:10 +0530

    bit_reverse of a number

---
+++ c programs/_bit_reverse_of_num.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+    int num,n,i,m,j;
+    printf("enter the number:");
+    scanf("%d",&num);
+    printf("before swap \n");
+    for(i=0;i<(sizeof(int)*8);i++)
+    printf("%d",num>>i&1);
+    printf("\n");
+
+    for(i=0,j=(sizeof(int)*8)-1;i<(sizeof(int)*8)/2;i++,j--)
+    {
+        if(i<j){
+        n=num&(1<<i);
+        m=num&(1<<j);
+        }
+        if(m!=n)
+        {
+            num = num^i<<1;
+            num = num^j<<1;
+        }
+    }
+        printf("after swap \n");
+    for(i=0;i<(sizeof(int)*8);i++)  //printing in big endian
+    printf("%d",num>>i&1);
+}
